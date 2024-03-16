e9b5aabffc93023e725c04f0935e2115fe5944da
colerusson <colerusson2@gmail.com>
2024-03-15 22:04:00 -0600

    initial commit

---
+++ settings.py
@@ -0,0 +1,18 @@
+# initialize Redis connection settings
+REDIS_HOST = "redis-18963.c124.us-central1-1.gce.cloud.redislabs.com"
+REDIS_PORT = 18963 # <-- likely need to change this too!
+REDIS_PASSWORD = "mHC9ZtLQkZRvnfRXJyHt4tkH8mDjjSC0"
+REDIS_DB = 0
+
+# initialize constants used to control image spatial dimensions and
+# data type
+IMAGE_WIDTH = 224
+IMAGE_HEIGHT = 224
+IMAGE_CHANS = 3
+IMAGE_DTYPE = "float32"
+
+# initialize constants used for server queuing
+IMAGE_QUEUE = "image_queue"
+BATCH_SIZE = 32
+SERVER_SLEEP = 0.25
+CLIENT_SLEEP = 0.25
